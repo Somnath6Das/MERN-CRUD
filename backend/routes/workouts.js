dc027aa63a669a76938ca5cf35f550541b4d25ce
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-08-10 00:52:08 +0530

    prt1

---
+++ backend/routes/workouts.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const {createWorkout,getWorkouts, getWorkout, deleteWorkout,updateWorkout } = require('./../controllers/workoutController')
+
+//? Get all workouts
+router.get('/', getWorkouts)
+
+
+//? Get a single workouts
+router.get('/:id',getWorkout)
+
+
+//? Post Req, create a new workout
+router.post('/',createWorkout)
+
+//? Delete a workout
+router.delete('/:id', deleteWorkout)
+
+//? Update a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
