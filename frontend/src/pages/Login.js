cae15818b3bf0aa8c2f29b7ddc964e6e3254515b
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-08-14 06:48:35 +0530

    loginJWT

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {login, error, isLoading} =useLogin()
 
     const handleSubmit = async (e) => {
         //prevent auto refresh to click submit
         e.preventDefault()
 
-        console.log(email, password)
+        await login(email, password)
     }
 
     return (
@@ -18,7 +20,8 @@ const Login = () => {
             <input type="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
             <label>Password:</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
-            <button>Log in</button>
+            <button disabled={isLoading}>Log in</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 
